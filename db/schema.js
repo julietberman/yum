f0daa739a5eaa303ebc0c69096fc2442702d83ab
Juliet Berman <berman.juliet@gmail.com>
2016-11-30 22:31:08 -0500

    initial commit and set up schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var ObjectId = Schema.ObjectId
+
+var MenuSchema = new mongoose.Schema({
+   title: String
+ })
+
+var RestaurantSchema = new mongoose.Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items:  [{type: ObjectId, ref: "MenuSchema"}]
+});
+
+mongoose.model("Restaurant", RestaurantSchema);
+mongoose.model("MenuItem", MenuSchema)
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require("express");
+var app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(4000, () => {
+  console.log("app listening on port 4000");
+});
